93c4adf18328c4d6f8b4c5b9bd504158771ef16a
João Victor Silva <39493542+joovictsousa@users.noreply.github.com>
2021-05-31 12:37:23 -0400

    Add files via upload

---
+++ Programação Competitiva/Atitmética Modular/2721.cpp
@@ -0,0 +1,18 @@
+ #include<bits/stdc++.h>
+ #define _ ios_base::sync_with_stdio(0);
+ 
+ using namespace std;
+ 
+ int main(){
+    string nomes[] = {"Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner" , "Blitzen"};
+    int num,soma=0;
+    
+    for(int i=0;i<9;i++){
+        cin >> num;
+        soma += num;
+    }
+    soma = soma%9;
+    cout << nomes[soma] << endl;
+ 
+    return 0;
+ }
